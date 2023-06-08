50b8529d1c158ee2999473691b6d6118ea80d156
Nuno Mendonça <nunomendonca95@gmail.com>
2023-06-09 00:05:35 +0100

    Sample the Pokemon

--- Pokedex3/ContentView.swift
+++ Pokedex3/ContentView.swift
@@ -9,24 +9,24 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    //Access viewContext of our environment (App)
     @Environment(\.managedObjectContext) private var viewContext
 
+    //To get our data from the Persistence. Ascending true: New values go to the bottom.
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
+        animation: .default) private var pokedex: FetchedResults<Pokemon>
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
+                ForEach(pokedex) { pokemon in
                     NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        Text("\(pokemon.id): \(pokemon.name!.capitalized)")
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        Text("\(pokemon.id): \(pokemon.name!.capitalized)")
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
             .toolbar {
 #if os(iOS)
@@ -34,57 +34,14 @@ struct ContentView: View {
                     EditButton()
                 }
 #endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        //Here we use the preview, so we can preview on the sim.
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
